a966d86cb4ceeefc4179637bc2b4319ab11167e6
tcui18 <tiffanyc.1262@gmail.com>
2022-05-13 15:42:32 +0000

    got through video to 1:10

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,14 @@
+class PhotosController < ApplicationController
+  def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({:created_at => :desc})
+    render({:template => "photo_templates/index.html.erb"})
+  end
+
+  def show
+    url_id = params.fetch("photo_id")
+    matching_photos = Photo.where({:id => url_id})
+    @the_photo = matching_photos[0]
+    render({:template => "photo_templates/show.html.erb"})
+  end
+end
